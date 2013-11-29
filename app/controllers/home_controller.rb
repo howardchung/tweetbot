592cade74a6f2f6f11b413e8116592802200cbca
Howard Chung <howardc93@gmail.com>
2013-11-29 09:13:13 -0500

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,39 @@
+class HomeController < ApplicationController
+
+    require 'twitter'
+
+    def index
+    if session['access_token'] && session['access_secret']
+              #@user = client.user(include_entities: true)
+              #@timeline=client.user_timeline(@user, :count=>3200, :include_rts=>false).map{|obj|obj.text}
+    else
+      render "signin"
+    end
+  end
+
+  def error
+    flash[:error] = "Sign in with Twitter failed"
+    redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: "Signed out"
+  end
+
+    def create
+    session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+    session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
+    redirect_to root_path, notice: "Signed in"
+  end
+
+  def client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.oauth_token = session['access_token']
+      config.oauth_token_secret = session['access_secret']
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetbot::Application.config.secret_key_base = '81416aa3f190c5b230baa01d850c062723a29f803fa2bdad02a72723599dc0456c69ff0a9d0e581410b85de8b7526389b7756f38e8d57e42ab881000ed93afd2'

79e5eacf66d37a778b8b84b3537cbe1fd313d427
Howard Chung <howardc93@gmail.com>
2013-11-30 18:42:19 -0500

    fixed bug, also nonduplicated tweets and last page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,15 @@ class HomeController < ApplicationController
     def index
     if session['access_token'] && session['access_secret']
             firstPage=client.user_timeline(:count=>200,:include_rts=>false)
-            @latest_tweets=prev.map{|obj|obj.text}
+            @latest_tweets=firstPage.map{|obj|obj.text}
             prev_id=0
             current_id=firstPage.last.id
             while prev_id !=current_id
-              nextPage=client.user_timeline(:count=>200, :max_id=>current_id,:include_rts=>false)
-              @latest_tweets.concat(nextPage.map{|obj|obj.text})
+              #last page is added twice
+              nextPage=client.user_timeline(:count=>200, :max_id=>current_id+1,:include_rts=>false)
               prev_id=current_id
               current_id=nextPage.last.id
+              @latest_tweets.concat(nextPage.map{|obj|obj.text}) if prev_id!=current_id
             end
     else
       render "signin"
